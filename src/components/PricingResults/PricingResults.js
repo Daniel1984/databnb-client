0ac96dd45999e2d2e842bdda3e64f9da8153c75b
daniel1984 <aciddaniel@gmail.com>
2017-12-19 01:21:25 +0200

    scrolling conten into viewport after response is returned

--- src/components/PricingResults/PricingResults.js
+++ src/components/PricingResults/PricingResults.js
@@ -1,19 +1,26 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import classnames from 'classnames';
 import MapInfoBlock from '../MapInfoBlock/MapInfoBlock';
 import QuickSummaryBlock from '../QuickSummaryBlock/QuickSummaryBlock';
 import styles from './PricingResults.scss';
 
-export default function PricingResults({ latlng, listings, bedrooms, address }) {
-  return (
-    <div class={classnames(styles.root, listings.length && styles.expanded)}>
-      <MapInfoBlock
-        latlng={latlng}
-        listings={listings}
-        bedrooms={bedrooms}
-        address={address}
-      />
-      <QuickSummaryBlock listings={listings} latlng={latlng} />
-    </div>
-  )
+export default class PricingResults extends Component{
+  componentWillReceiveProps() {
+    this.rootEl.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+
+  render() {
+    const { latlng, listings, bedrooms, address } = this.props;
+    return (
+      <div ref={el => this.rootEl = el} class={classnames(styles.root, listings.length && styles.expanded)}>
+        <MapInfoBlock
+          latlng={latlng}
+          listings={listings}
+          bedrooms={bedrooms}
+          address={address}
+        />
+        <QuickSummaryBlock listings={listings} latlng={latlng} />
+      </div>
+    );
+  }
 }
