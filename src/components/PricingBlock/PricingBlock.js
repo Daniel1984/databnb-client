3566f198578e964d7778c60994f8e185decd2106
daniel1984 <aciddaniel@gmail.com>
2017-12-22 14:05:38 +0200

    restyling of pricing page

--- src/components/PricingBlock/PricingBlock.js
+++ src/components/PricingBlock/PricingBlock.js
@@ -11,51 +11,72 @@ function PricingBlock() {
       <div class={styles.pricingContainer}>
         <div class={styles.pricingBlock}>
           <div class={styles.heading}>
-            <div>
-              <div class={styles.pricingTitle}>Free</div>
-              <div class={styles.cost}>$0</div>
-            </div>
-            <div>Features:</div>
+            <div class={styles.pricingTitle}>Free</div>
+            <div class={styles.cost}>$0</div>
           </div>
-          <ul class={styles.list}>
-            <li>Access to custom charts</li>
-            <li>Up to 10 queries per hour</li>
-          </ul>
+          <div class={styles.features}>
+            <div class={styles.featuresTitle}>
+              Features:
+            </div>
+            <div class={styles.feature}>
+              Access to custom charts
+            </div>
+            <div class={styles.feature}>
+              Up to 10 queries per hour
+            </div>
+          </div>
         </div>
 
-        <div class={classnames([styles.pricingBlock, styles.basicPricingBlock])}>
-          <div class={styles.heading}>
-            <div>
-              <div class={classnames([styles.pricingTitle, styles.popularTile])}>
-                Basic
-              </div>
-              <div class={classnames([styles.cost, styles.popularCost])}>
-                $10
-              </div>
+        <div class={styles.pricingBlock}>
+          <div class={classnames([styles.heading, styles.basicList])}>
+            <div class={classnames([styles.pricingTitle, styles.popularTile])}>
+              Basic
             </div>
-            <div>Features:</div>
+            <div class={classnames([styles.cost, styles.popularCost])}>
+              $10
+            </div>
           </div>
-          <ul class={classnames([styles.list, styles.basicList])}>
-            <li>Access to even more custom charts</li>
-            <li>Up to 50 queries per hour</li>
-            <li>Weekly personalized email reports</li>
-            <li>Weekly pricing recommendations</li>
-          </ul>
+          <div class={styles.features}>
+            <div class={styles.featuresTitle}>
+              Features:
+            </div>
+            <div class={styles.feature}>
+              Access to even more custom charts
+            </div>
+            <div class={styles.feature}>
+              Up to 50 queries per hour
+            </div>
+            <div class={styles.feature}>
+              Weekly personalized email reports
+            </div>
+            <div class={styles.feature}>
+              Weekly pricing recommendations
+            </div>
+          </div>
         </div>
 
         <div class={styles.pricingBlock}>
           <div class={styles.heading}>
-            <div>
-              <div class={styles.pricingTitle}>Pro</div>
-              <div class={styles.cost}>$20</div>
-            </div>
-            <div>Features:</div>
+            <div class={styles.pricingTitle}>Pro</div>
+            <div class={styles.cost}>$20</div>
           </div>
-          <ul class={styles.list}>
-            <li>All basic features</li>
-            <li>Unlimited queries</li>
-            <li>Download CSV/JSON data</li>
-          </ul>
+          <div class={styles.features}>
+            <div class={styles.featuresTitle}>
+              Features:
+            </div>
+            <div class={styles.feature}>
+              All basic features
+            </div>
+            <div class={styles.feature}>
+              More custom dashboards
+            </div>
+            <div class={styles.feature}>
+              Unlimited queries
+            </div>
+            <div class={styles.feature}>
+              Download CSV/JSON data
+            </div>
+          </div>
         </div>
       </div>
     </div>
