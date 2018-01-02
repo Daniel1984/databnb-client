91cc40cdc025030ae864a414dff8e9fb232a469c
daniel1984 <aciddaniel@gmail.com>
2018-01-03 01:40:12 +0200

    bugstang and gtag integration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import bugsnag from 'bugsnag-js'
 import { h, render } from 'preact';
 import App from './components/App/App';
+
+const client = bugsnag('48900f3e09cc8859e1e9220b2439f97a');
 
 render(<App />, document.body);
